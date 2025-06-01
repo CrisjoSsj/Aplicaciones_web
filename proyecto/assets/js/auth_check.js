c9a7060b4b73c93acceea158d68f601dddcb8f76
CrisjoSsj <crisjcq2004@gmail.com>
2025-06-01 15:30:16 -0500

    Delete proyecto directory

--- proyecto/assets/js/auth_check.js
+++
@@ -1,8 +0,0 @@
-// Verificar si el usuario está autenticado
-document.addEventListener('DOMContentLoaded', () => {
-    const usuario = localStorage.getItem('usuario');
-    if(!usuario) {
-      window.location.href = 'login.html';
-    }
-  });
-  
